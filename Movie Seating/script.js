186288475f5136a2684b31f11ad662414e50e7ea
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-04-28 13:39:33 -0700

    Added Movie Seating Project

---
+++ Movie Seating/script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.seating');
+const selectedSeats = document.getElementById("selected-seats");
+const totalPrice = document.getElementById('total');
+const currentMovie = document.getElementById('movies');
+let ticketPrice = +currentMovie.value;
+
+function update(){
+    const numOfSelectedSeats = document.querySelectorAll('.rows .seat.selected').length;
+    selectedSeats.innerText = numOfSelectedSeats;
+
+    totalPrice.innerText = ticketPrice * numOfSelectedSeats;
+}
+
+currentMovie.addEventListener('change', e=> {
+    ticketPrice = e.target.value;
+    update();
+})
+// Could use toggle instead of updating each class and removing 'selected' class.
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied'))
+    {
+        e.target.classList.toggle('selected');
+        update();
+    }
+})
