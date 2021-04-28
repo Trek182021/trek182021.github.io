4d20f924657efba6b755ccf91907267b36fec012
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-04-27 21:02:17 -0700

    Added Separate JS File and Transferred JS Script

---
+++ ProductLandingPage/script.js
@@ -0,0 +1,12 @@
+function changeRank(){
+    document.getElementById("rank").innerHTML="Silver";
+    document.getElementById("rank").style.color = "#C36902";
+}
+function changeRank2(){
+    document.getElementById("rank").innerHTML="Wizard";
+    document.getElementById("rank").style.color = "#6857AB";
+}
+function changeRank3(){
+    document.getElementById("rank").innerHTML="Dragon";
+    document.getElementById("rank").style.color = "#9A0026";
+}
