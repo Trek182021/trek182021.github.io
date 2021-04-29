4bdd814afd739c5b05906a3561833df4825c9cf2
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-04-28 23:48:18 -0700

    Add populateUI function, populates selected seat

--- MovieSeatingPage/script.js
+++ MovieSeatingPage/script.js
@@ -1,18 +1,42 @@
 const container = document.querySelector('.seating');
 const selectedSeats = document.getElementById("selected-seats");
+const seats = document.querySelectorAll('.rows .seat:not(.occupied)');
 const totalPrice = document.getElementById('total');
 const currentMovie = document.getElementById('movies');
 let ticketPrice = +currentMovie.value;
 
+populateUI();
+
+function setSeatIndex() {
+    const selectedSeats = document.querySelectorAll('.rows .seat.selected');
+
+    const sSeatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    console.log(sSeatIndex);
+    localStorage.setItem('selectedSeatIndex', JSON.stringify(sSeatIndex));
+}
+
+function saveMovieIP(movieIndex, moviePrice){
+    localStorage.setItem('currentMovieIndex', movieIndex);
+    localStorage.setItem('currentMoviePrice', moviePrice);
+}
 function update(){
     const numOfSelectedSeats = document.querySelectorAll('.rows .seat.selected').length;
     selectedSeats.innerText = numOfSelectedSeats;
     totalPrice.innerText = ticketPrice * numOfSelectedSeats;
 }
 
+function populateUI() {
+    const seatIndex = JSON.parse(localStorage.getItem('selectedSeatIndex'));
+    seatIndex.forEach(element => seats[element].classList.add('selected'));
+    ticketPrice = localStorage.getItem('currentMoviePrice');
+    currentMovie.selectedIndex = [localStorage.getItem('currentMovieIndex')];
+}
+
 currentMovie.addEventListener('change', e=> {
     ticketPrice = e.target.value;
     update();
+    saveMovieIP(e.target.selectedIndex,e.target.value);
 })
 // Could use toggle instead of updating each class and removing 'selected' class.
 container.addEventListener('click', e => {
@@ -20,5 +44,6 @@ container.addEventListener('click', e => {
     {
         e.target.classList.toggle('selected');
         update();
+        setSeatIndex();
     }
 })
